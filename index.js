bfa181d0fe8ba34bc3cb44066a602674c563c58c
sandy <sandyks.lo27@gmail.com>
2021-08-31 10:01:23 -0700

    commit tree ad515c01e1ae91faacfae2ac445e48beb3206d8b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+let saturdayFun = (activity = "roller-skate") => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = (flair = "*") => {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
